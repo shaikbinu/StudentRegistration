9fe9704a2468759668f8e32179afd2f096daf130
Shaik Binu <shaikbinu@gmail.com>
2021-09-14 09:18:20 +0000

    new line

--- Data/dbo.student_Data.sql
+++ Data/dbo.student_Data.sql
@@ -1,3 +1,4 @@
 INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('200', 'ew', 'IT', '1', 'shaikbinu@gmail.com', '12345')
 INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('100', 'ew', 'IT', '1', 'shaikbinu@gmail.com', '12345')
 INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('500', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
+INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('600', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
