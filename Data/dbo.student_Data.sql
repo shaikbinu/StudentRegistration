d9e12d31db35452d4cc12ed302483f8359eb8106
Shaik Binu <shaikbinu@gmail.com>
2021-09-14 08:31:23 +0000

    SQL Change

--- Data/dbo.student_Data.sql
+++ Data/dbo.student_Data.sql
@@ -1,1 +1,1 @@
-INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('2', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
+INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('5', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
