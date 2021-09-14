c5cd675129246b07b0e3e683fb1ed070bc86785e
Shaik Binu <shaikbinu@gmail.com>
2021-09-14 09:02:37 +0000

    Check Nginix Pull

--- Data/dbo.student_Data.sql
+++ Data/dbo.student_Data.sql
@@ -1,3 +1,3 @@
 INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('20', 'ew', 'IT', '1', 'shaikbinu@gmail.com', '12345')
-INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('10', 'ew', 'IT', '1', 'shaikbinu@gmail.com', '12345')
-INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('5', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
+INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('100', 'ew', 'IT', '1', 'shaikbinu@gmail.com', '12345')
+INSERT INTO [dbo].[student] ([student_id], [student_name], [course], [address], [email], [phone]) VALUES ('500', 'Shaik Binu', 'IT', '1', 'shaikbinu@gmail.com', '12345')
